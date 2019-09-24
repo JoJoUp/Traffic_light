d0e44a70474dc50f6b4992ba7ab560c8e2b13243
Chance <playgamestimes@gmail.com>
2019-09-24 12:49:02 +0300

    i
    int

---
+++ app/src/main/java/com/example/svetofor/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.svetofor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout myConstraintLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myConstraintLayout = findViewById(R.id.constraintLayout);
+    }
+
+
+    public void clickOnYellowButton (View view){
+        myConstraintLayout.setBackgroundColor(getResources().getColor(R.color.colorYellow));
+    }
+    public void clickOnGreenButton (View view){
+        myConstraintLayout.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+    }
+
+    public void clickOnRedButton(View view) {
+        myConstraintLayout.setBackgroundColor(getResources().getColor(R.color.colorRed));
+    }
+}
